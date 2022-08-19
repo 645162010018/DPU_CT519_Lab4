93b5d418502d394d97881902795c877ae348ab61
645162010018 <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>
2022-08-19 14:53:48 +0700

    initial commit

---
+++ www/about.php
@@ -0,0 +1,11 @@
+<html>
+        <head>
+                <title>Lab4</title>
+        </head>
+        <body
+        <p>Name : Papitchaya Sriserm</p><br/>
+        <p>ID : 645162010018</p><br/>
+        <a href="index.php">Back</a>
+        </body>
+</html>
+
